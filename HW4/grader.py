895edd31027d27ae3a8880e9c7461016de8a1f58
SNOWKIM <rlatnsgh0708@postech.ac.kr>
2024-04-11 10:33:52 +0900

    upload HW4

---
+++ HW4/grader.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+
+import random, util, collections
+import graderUtil
+import numpy as np
+
+grader = graderUtil.Grader()
+submission = grader.load('submission')
+
+try:
+    import solution
+    grader.addHiddenPart = grader.addBasicPart
+    SEED = solution.SEED
+    solution_exist = True
+except ModuleNotFoundError:
+    SEED = 42
+    solution_exist = False
+
+def test_correct(func_name, assertion=lambda pred: True, equal=lambda x, y: x == y):
+    def test():
+        pred = getattr(submission, func_name)()
+        assert pred is None or assertion(pred)
+        if solution_exist:
+            answer = getattr(solution, func_name)()
+            grader.requireIsTrue(equal(pred, answer))
+    return test
+
+def test_wrong(func_name, assertion=lambda pred: True):
+    def test():
+        pred = getattr(submission, func_name)()
+        assert pred is None or assertion(pred)
+        if solution_exist:
+            answer = getattr(solution, func_name)()
+            grader.requireIsTrue(pred != answer and pred is not None)
+    return test
+
+# 2024.03.26. 
+############################################################
+# Problem 1
+
+def test_1_1():
+    tests =[[
+                np.array([[0, 0, -50, 20],
+                          [0, 0, -50,  0],
+                          [2, 0,   0,  0]], 
+                          dtype = np.float64),
+                1, -1, 10,
+                np.array([[13, 14, -50, 20],
+                          [14, 15, -50, 19],
+                          [ 2, 16,  17, 18]], 
+                          dtype = np.float64)
+            ],
+
+            [np.array([[0, -50, 10, 0],
+                       [0,   0,-30, 0],
+                       [0,   0,  0, 0]]),
+                1, -1, 10,
+                np.array([[2, -50,  10,   9],
+                          [3,   4, -30,   8],
+                          [4,   5,   6,   7]], 
+                          dtype = np.float64)
+            ]
+    ]
+    for grid_world, discount, moveReward, numIters, answer in tests:
+        volcano_crossing = submission.VolcanoCrossing(grid_world, discount=discount, moveReward=moveReward)
+        grader.requireIsEqual(answer, volcano_crossing.value_iteration(numIters))
+grader.addBasicPart('1a-1-basic', test_1_1, 3, description="Basic test for VolcanoCrossing.")
+
+def test_1_2():
+    tests =[[np.array([[0, 0, -50, 20],
+                    [0, 0, -50,  0],
+                    [2, 0,   0,  0]], dtype = np.float64),
+            0.9, -0.7, 10],
+
+            [np.array([[0, -50, 10, 0],
+                    [0,   0,-30, 0],
+                    [0,   0,  0, 0]], dtype = np.float64),
+            0.5, -0.3, 10]
+    ]
+    if solution_exist:
+        for grid_world, discount, moveReward, numIters in tests:
+            answer = solution.VolcanoCrossing(grid_world, discount=discount, moveReward=moveReward)
+            pred = submission.VolcanoCrossing(grid_world, discount=discount, moveReward=moveReward)
+            grader.requireIsEqual(answer.value_iteration(numIters), pred.value_iteration(numIters))
+
+grader.addHiddenPart('1a-2-hidden', test_1_2, 2, description="Hidden test for VolcanoCrossing.")
+
+############################################################
+# Problem 2
+
+def test_2a_1():
+    mdp1 = submission.BlackjackMDP(cardValues=[1, 5], multiplicity=2,
+                                   threshold=10, peekCost=1)
+    startState = mdp1.startState()
+    preBustState = (6, None, (1, 1))
+    postBustState = (11, None, None)
+
+    mdp2 = submission.BlackjackMDP(cardValues=[1, 5], multiplicity=2,
+                                   threshold=15, peekCost=1)
+    preEmptyState = (11, None, (1,0))
+
+    # Make sure the succAndProbReward function is implemented correctly.
+    tests = [
+        ([((1, None, (1, 2)), 0.5, 0), ((5, None, (2, 1)), 0.5, 0)], mdp1, startState, 'Take'),
+        ([((0, 0, (2, 2)), 0.5, -1), ((0, 1, (2, 2)), 0.5, -1)], mdp1, startState, 'Peek'),
+        ([((0, None, None), 1, 0)], mdp1, startState, 'Quit'),
+        ([((7, None, (0, 1)), 0.5, 0), ((11, None, None), 0.5, 0)], mdp1, preBustState, 'Take'),
+        ([], mdp1, postBustState, 'Take'),
+        ([], mdp1, postBustState, 'Peek'),
+        ([], mdp1, postBustState, 'Quit'),
+        ([((12, None, None), 1, 12)], mdp2, preEmptyState, 'Take')
+    ]
+    for gold, mdp, state, action in tests:
+        if not grader.requireIsEqual(gold,
+                                     mdp.succAndProbReward(state, action)):
+            print('   state: {}, action: {}'.format(state, action))
+grader.addBasicPart('2a-1-basic', test_2a_1, 3, description="Basic test for succAndProbReward() that covers several edge cases.")
+
+def test_2a_2():
+    def solve(BlackjackMDP):
+        mdp = BlackjackMDP(cardValues=[1, 3, 5, 8, 10], multiplicity=3,
+                           threshold=40, peekCost=1)
+        startState = mdp.startState()
+        alg = util.ValueIteration()
+        alg.solve(mdp, .0001)
+        return alg.V[startState]
+
+    pred = solve(submission.BlackjackMDP)
+    if solution_exist:
+        answer = solve(solution.BlackjackMDP)
+        grader.requireIsTrue((abs(pred - answer) / answer) < 0.1)
+        
+grader.addHiddenPart('2a-2-hidden', test_2a_2, 2, description="Hidden test for ValueIteration. Run ValueIteration on BlackjackMDP, then test if V[startState] is correct.")
+
+############################################################
+# Problem 3
+
+def test_3a_1():
+    mdp = util.NumberLineMDP()
+    rl = submission.Qlearning(mdp.actions, mdp.discount(),
+                              submission.identityFeatureExtractor,
+                              0)
+    # We call this here so that the stepSize will be 1
+    rl.numIters = 1
+
+    rl.incorporateFeedback([0, 1, 0, 1], mdp.isEnd)
+    grader.requireIsEqual(0, rl.getQ(0, -1))
+    grader.requireIsEqual(0, rl.getQ(0, 1))
+
+    rl.incorporateFeedback([1, 1, 1, 2], mdp.isEnd)
+    grader.requireIsEqual(0, rl.getQ(0, -1))
+    grader.requireIsEqual(0, rl.getQ(0, 1))
+    grader.requireIsEqual(0, rl.getQ(1, -1))
+    grader.requireIsEqual(1, rl.getQ(1, 1))
+
+    rl.incorporateFeedback([2, -1, 1, 1], mdp.isEnd)
+    grader.requireIsEqual(1.9, rl.getQ(2, -1))
+    grader.requireIsEqual(0, rl.getQ(2, 1))
+
+grader.addBasicPart('3a-1-basic', test_3a_1, 3, maxSeconds=3, description="Basic test for incorporateFeedback() using NumberLineMDP.")
+
+
+def test_3a_2():
+    if solution_exist:
+        BlackjackMDP = solution.BlackjackMDP
+    else:
+        BlackjackMDP = submission.BlackjackMDP
+    mdp = BlackjackMDP(cardValues=[1, 5], multiplicity=2, threshold=10, peekCost=1)
+
+    def get_policy(Qlearning):
+        rl = Qlearning(mdp.actions, mdp.discount(),
+                       lambda state, action: [((state, action), 1)],
+                       0.2)  # 0.2 is the default epsilon
+        random.seed(SEED)
+        util.simulate(mdp, rl, numTrials=15000)
+        rl.explorationProb = 0.0
+        policy = {state: rl.getAction(state) for state in mdp.states}
+        return policy
+
+    pred = get_policy(submission.Qlearning)
+
+    if solution_exist:
+        answer = get_policy(solution.Qlearning)
+
+        all_states = [state for state in mdp.states
+                      if not mdp.isEnd(state)]
+        grader.requireIsTrue((sum(int(pred[state] == answer[state])
+                                  for state in all_states) / len(all_states)) > 0.95)
+        
+grader.addHiddenPart('3a-2-hidden', test_3a_2, 2, maxSeconds=3, description="Hidden test for incorporateFeedback(). Run Qlearning on a small MDP, then ensure that getQ returns reasonable policy.")
+
+def test_3b_1():
+    mdp = util.NumberLineMDP()
+    rl = submission.SARSA(mdp.actions, mdp.discount(),
+                          submission.identityFeatureExtractor,
+                          0)
+    # We call this here so that the stepSize will be 1
+    rl.numIters = 1
+
+    rl.incorporateFeedback([0, 1, 0, 1, 1, None, None], mdp.isEnd)
+    grader.requireIsEqual(0, rl.getQ(0, -1))
+    grader.requireIsEqual(0, rl.getQ(0, 1))
+
+    rl.incorporateFeedback([1, 1, 1, 2, -1, None, None], mdp.isEnd)
+    grader.requireIsEqual(0, rl.getQ(0, -1))
+    grader.requireIsEqual(0, rl.getQ(0, 1))
+    grader.requireIsEqual(0, rl.getQ(1, -1))
+    grader.requireIsEqual(1, rl.getQ(1, 1))
+
+    rl.incorporateFeedback([2, -1, 1, 1, 1, None, None], mdp.isEnd)
+    grader.requireIsEqual(1.9, rl.getQ(2, -1))
+    grader.requireIsEqual(0, rl.getQ(2, 1))
+
+grader.addBasicPart('3b-1-basic', test_3b_1, 3, maxSeconds=3, description="Basic test for incorporateFeedback() using NumberLineMDP.")
+
+
+def test_3b_2():
+    if solution_exist:
+        BlackjackMDP = solution.BlackjackMDP
+    else:
+        BlackjackMDP = submission.BlackjackMDP
+    mdp = BlackjackMDP(cardValues=[1, 5], multiplicity=2, threshold=10, peekCost=1)
+
+    def get_policy(SARSA):
+        rl = SARSA(mdp.actions, mdp.discount(),
+                                lambda state, action: [((state, action), 1)],
+                                0.2)  # 0.2 is the default epsilon
+        random.seed(SEED)
+        util.simulate(mdp, rl, numTrials=15000)
+        rl.explorationProb = 0.0
+        policy = {state: rl.getAction(state) for state in mdp.states}
+        return policy
+
+    pred = get_policy(submission.SARSA)
+
+    if solution_exist:
+        answer = get_policy(solution.SARSA)
+
+        all_states = [state for state in mdp.states
+                      if not mdp.isEnd(state)]
+        grader.requireIsTrue((sum(int(pred[state] == answer[state])
+                                  for state in all_states) / len(all_states)) > 0.95)
+        
+grader.addHiddenPart('3b-2-hidden', test_3b_2, 2, maxSeconds=3, description="Hidden test for incorporateFeedback(). Run SARSA on a small MDP, then ensure that getQ returns reasonable policy.")
+
+def test_3c_1():
+    mdp = submission.BlackjackMDP(cardValues=[1, 5], multiplicity=2,
+                                  threshold=10, peekCost=1)
+    rl = submission.Qlearning(mdp.actions, mdp.discount(),
+                              submission.blackjackFeatureExtractor,
+                              0)
+    # We call this here so that the stepSize will be 1
+    rl.numIters = 1
+
+    rl.incorporateFeedback([(7, None, (0, 1)), 'Quit', 7, (7, None, None)], mdp.isEnd)
+    grader.requireIsEqual(28, rl.getQ((7, None, (0, 1)), 'Quit'))
+    grader.requireIsEqual(7, rl.getQ((7, None, (1, 0)), 'Quit'))
+    grader.requireIsEqual(14, rl.getQ((2, None, (0, 2)), 'Quit'))
+    grader.requireIsEqual(0, rl.getQ((2, None, (0, 2)), 'Take'))
+grader.addBasicPart('3c-1-basic', test_3c_1, 3, maxSeconds=3, description="Basic test for blackjackFeatureExtractor.  Runs Qlearning using blackjackFeatureExtractor, then checks to see that Q-values are correct.")
+
+def test_3c_2():
+    if solution_exist:
+        BlackjackMDP = solution.BlackjackMDP
+        Qlearning = solution.Qlearning
+    else:
+        BlackjackMDP = submission.BlackjackMDP
+        Qlearning = submission.Qlearning
+
+    mdp = BlackjackMDP(cardValues=[1, 3, 5, 8, 10], multiplicity=3, threshold=40, peekCost=1)
+
+    rl = Qlearning(mdp.actions, mdp.discount(),
+                   submission.blackjackFeatureExtractor,
+                   0.2)  # 0.2 is the default epsilon
+    random.seed(SEED)
+    util.simulate(mdp, rl, numTrials=30000)
+    rl.explorationProb = 0.0
+    rl_policy = {state: rl.getAction(state) for state in mdp.states}
+
+    vi = util.ValueIteration()
+    vi.solve(mdp)
+    vi_policy = vi.pi
+    
+    all_states = [state for state in mdp.states
+                  if not mdp.isEnd(state)]
+    agreement = sum(int(rl_policy[state] == vi_policy[state])
+                    for state in all_states) / len(all_states)
+    grader.requireIsTrue(agreement > 0.7)
+    print('Policy agreement:', agreement)
+
+grader.addHiddenPart('3c-2-hidden', test_3c_2, 2, maxSeconds=20, description="Hidden test for incorporateFeedback(). Run Qlearning on a large MDP, then ensure that getQ returns reasonable policy.")
+
+
+grader.grade()
---
+++ HW4/submission.py
@@ -0,0 +1,225 @@
+import util, math, random
+from collections import defaultdict
+from util import ValueIteration
+import numpy as np
+
+
+############################################################
+# Problem 1a: Volcano Crossing
+
+
+class VolcanoCrossing():
+    """
+    grid_world: a 2D numpy array where 0 is explorable, negative integer is a volcano, and positive integer is the goal.
+    discount: discount factor
+    moveReward: reward of moving from one cell to another
+    value_table: a 2D numpy array where each cell represents the value of the cell
+    actions: a list of possible actions
+    """
+    def __init__(self, grid_world, discount=1, moveReward=-1):
+        self.grid_world = grid_world
+        self.discount = discount
+        self.moveReward = moveReward
+        self.actions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    # Return the value table after running |numIters| of value iteration.
+    # You do not need to modify this function.
+    def value_iteration(self, numIters=1):
+        self.value_table = np.zeros(self.grid_world.shape) # Initialize value table
+
+        for _ in range(numIters):
+            self.value_table = self.value_update(self.value_table)
+        return self.value_table
+
+    # Return the state is Volcano or Island.
+    # You do not need to modify this function.
+    # If the state is Volcano or Island, return True.
+    # Otherwise(self.grid_world[state] == 0), return False.
+    # This function below has been implemented for your convenience, but it is not necessarily required to be used.
+    def is_volcano_or_island(self, state):
+        return self.grid_world[state] != 0
+
+    # Checks if the agent can move to the next state.
+    # This function below has been implemented for your convenience, but it is not necessarily required to be used.
+    def movable(self, state, action):
+        x, y = state
+        i, j = action
+        return 0 <= x + i < self.grid_world.shape[0] and 0 <= y + j < self.grid_world.shape[1]
+
+
+    # Return the value table after updating the value of each grid cell.
+    def value_update(self, value_table):
+        # BEGIN_YOUR_ANSWER (our solution is 14 lines of code, but don't worry if you deviate from this)
+        rows, cols = value_table.shape        
+        for x in range(rows):
+            for y in range (cols):
+                state = (x, y)
+                if self.is_volcano_or_island(state) :
+                    value_table[state] = self.grid_world[state]
+                else :
+                    # volcano와 island가 아닌 모든 state에 대해서
+                    maxValue = -np.inf
+                    # 4가지 action에 대한 Value를 계산하고 최댓값을 저장
+                    for action in self.actions :
+                        if self.movable(state, action) :
+                            succ = (state[0]+action[0], state[1]+action[1])
+                            Qopt = self.moveReward + self.discount * value_table[succ] 
+                            maxValue = max(Qopt, maxValue)
+                    value_table[state] = maxValue        
+        return value_table
+        # END_YOUR_ANSWER
+
+############################################################
+# Problem 2a: BlackjackMDP
+
+
+class BlackjackMDP(util.MDP):
+    def __init__(self, cardValues, multiplicity, threshold, peekCost):
+        """
+        cardValues: array of card values for each card type
+        multiplicity: number of each card type
+        threshold: maximum total before going bust
+        peekCost: how much it costs to peek at the next card
+        """
+        super().__init__()
+
+        self.cardValues = cardValues
+        self.multiplicity = multiplicity
+        self.threshold = threshold
+        self.peekCost = peekCost
+
+    # Return the start state.
+    # Look at this function to learn about the state representation.
+    # The first element of the tuple is the sum of the cards in the player's
+    # hand.
+    # The second element is the index (not the value) of the next card, if the player peeked in the
+    # last action.  If they didn't peek, this will be None.
+    # The final element is the current deck.
+    def startState(self):
+        return (0, None, (self.multiplicity,) * len(self.cardValues))  # total, next card (if any), multiplicity for each card
+
+    # Return set of actions possible from |state|.
+    # You do not need to modify this function.
+    # All logic for dealing with end states should be done in succAndProbReward
+    def actions(self, state):
+        return ['Take', 'Peek', 'Quit']
+
+    # Return a list of (newState, prob, reward) tuples corresponding to edges
+    # coming out of |state|.  Indicate a terminal state (after quitting or
+    # busting) by setting the deck to None. 
+    # When the probability is 0 for a particular transition, don't include that 
+    # in the list returned by succAndProbReward.
+    def succAndProbReward(self, state, action):
+        # BEGIN_YOUR_ANSWER (our solution is 44 lines of code, but don't worry if you deviate from this)
+        raise NotImplementedError  # remove this line before writing code
+        # END_YOUR_ANSWER
+
+    def discount(self):
+        return 1
+
+
+############################################################
+# Problem 3a: Q learning
+
+# Performs Q-learning.  Read util.RLAlgorithm for more information.
+# actions: a function that takes a state and returns a list of actions.
+# discount: a number between 0 and 1, which determines the discount factor
+# featureExtractor: a function that takes a state and action and returns a list of (feature name, feature value) pairs.
+# explorationProb: the epsilon value indicating how frequently the policy
+# returns a random action
+class Qlearning(util.RLAlgorithm):
+    def __init__(self, actions, discount, featureExtractor, explorationProb=0.2):
+        self.actions = actions
+        self.discount = discount
+        self.featureExtractor = featureExtractor
+        self.explorationProb = explorationProb
+        self.weights = defaultdict(float)
+        self.numIters = 0
+
+    # Return the Q function associated with the weights and features
+    def getQ(self, state, action):
+        score = 0
+        for f, v in self.featureExtractor(state, action):
+            score += self.weights[f] * v
+        return score
+
+    # This algorithm will produce an action given a state.
+    # Here we use the epsilon-greedy algorithm: with probability
+    # |explorationProb|, take a random action.
+    def getAction(self, state):
+        self.numIters += 1
+        if random.random() < self.explorationProb:
+            return random.choice(self.actions(state))
+        else:
+            return max((self.getQ(state, action), action) for action in self.actions(state))[1]
+
+    # Call this function to get the step size to update the weights.
+    def getStepSize(self):
+        return 1.0 / math.sqrt(self.numIters)
+
+    # We will call this function with episode=[..., state, action,
+    # reward, newState], which you should use to update
+    # |self.weights|. You should update |self.weights| using
+    # self.getStepSize(); use self.getQ() to compute the current
+    # estimate of the parameters. Also, you should assume that
+    # V_opt(newState)=0 when isLast(newState) is True
+    def incorporateFeedback(self, episode, isLast):
+        state, action, reward, newState = episode[-4:]
+
+        if isLast(state):
+            return
+
+        # BEGIN_YOUR_ANSWER (our solution is 8 lines of code, but don't worry if you deviate from this)
+        raise NotImplementedError  # remove this line before writing code
+        # END_YOUR_ANSWER
+
+
+############################################################
+# Problem 3b: Q SARSA
+
+class SARSA(Qlearning):
+    # We will call this function with episode=[..., state, action,
+    # reward, newState, newAction, newReward, newNewState], which you
+    # should use to update |self.weights|. You should
+    # update |self.weights| using self.getStepSize(); use self.getQ()
+    # to compute the current estimate of the parameters. Also, you
+    # should assume that Q_pi(newState, newAction)=0 when when
+    # isLast(newState) is True
+    def incorporateFeedback(self, episode, isLast):
+        assert (len(episode) - 1) % 3 == 0
+        if len(episode) >= 7:
+            state, action, reward, newState, newAction = episode[-7: -2]
+        else:
+            return
+
+        # BEGIN_YOUR_ANSWER (our solution is 8 lines of code, but don't worry if you deviate from this)
+        raise NotImplementedError  # remove this line before writing code
+        # END_YOUR_ANSWER
+
+# Return a singleton list containing indicator feature (if exist featurevalue = 1)
+# for the (state, action) pair.
+def identityFeatureExtractor(state, action):
+    featureKey = (state, action)
+    featureValue = 1
+    return [(featureKey, featureValue)]
+
+############################################################
+# Problem 3c: features for Q-learning.
+
+# You should return a list of (feature key, feature value) pairs
+# (see identityFeatureExtractor() above for an example).
+# Implement the following features:
+# - indicator on the total and the action (1 feature).
+# - indicator on the presence/absence of each card type and the action (1 feature).
+#       Example: if the deck is (3, 4, 0 , 2), then your indicator on the presence of each card type is (1,1,0,1)
+#       Only add this feature if the deck != None
+# - indicator on the number of cards for each card type and the action (len(counts) features).
+#       Example: if the deck is (3, 4, 0, 2), you should have four features (one for each card type).
+#       And the first feature key will be (0, 3, action)
+#       Only add these features if the deck != None
+
+def blackjackFeatureExtractor(state, action):
+    total, nextCard, counts = state
+    # BEGIN_YOUR_ANSWER (our solution is 8 lines of code, but don't worry if you deviate from this)
+    raise NotImplementedError  # remove this line before writing code
+    # END_YOUR_ANSWER
